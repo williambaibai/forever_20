7214639ee63e8b3936f9c7fd08524f30d043741c
William Bai <williambaibai@gmail.com>
2019-04-12 01:16:48 -0400

    Add data

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,4 +1,8 @@
 from . import *  
+
+import numpy as np
+import pandas as pd
+
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
@@ -9,9 +13,25 @@ net_id = "HaiYang Bai (hb388),\
 		  Yue Gao (yg98),\
 		  Zidong Zheng (zz357)"
 
+# open csv files
+cleanser_db = pd.read_csv('data/cleanser_db.csv')
+eye_care_db = pd.read_csv('data/eye_care_db.csv')
+lip_treatment_db = pd.read_csv('data/lip_treatment_db.csv')
+masks_db = pd.read_csv('data/masks_db.csv')
+moisturizer_db = pd.read_csv('data/moisturizer_db.csv')
+sun_care_db = pd.read_csv('data/sun_care_db.csv')
+treatment_db = pd.read_csv('data/treatment_db.csv')
+
+# TODO: organize and process data
+
 @irsystem.route('/', methods=['GET'])
 def search():
+	# get search parameters
 	query = request.args.get('search')
+	category = request.args.get('category')
+	price_range = request.args.get('price_range')
+	# TODO: add search parameters
+
 	if not query:
 		data = []
 		output_message = ''
